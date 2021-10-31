7dc0e98f120ed98848b1f7216198c9c7dcbdb9e9
OValya <valya.eto.ya@gmail.com>
2021-10-31 15:51:26 +0300

    feat: done 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,15 @@
+const stdout = process.stdout;
+const stdin = process.stdin;
+const path = require('path');
+const fs = require('fs');
+const output = fs.createWriteStream(path.join(path.dirname(__filename), 'text.txt'));
+stdout.write(`Hello! \nEnter text:\n`);
+//console.log(path.dirname(__filename));
+stdin.on('data', (partData) =>{
+    if(partData.toString().includes('exit')) 
+        process.exit();
+    else {
+        output.write(partData);
+    }
+});
+process.on('exit', ()=> stdout.write('See you later!'))
